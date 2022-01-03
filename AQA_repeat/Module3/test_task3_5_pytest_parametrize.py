77ec362dc442b94f2f5365697d55d4c272db25d6
“Eugene <zhenyamoroz2208@gmail.com>
2022-01-03 16:01:44 +0200

    add tests

--- AQA_repeat/Module3/test_task3_5_pytest_parametrize.py
+++ AQA_repeat/Module3/test_task3_5_pytest_parametrize.py
@@ -1,3 +1,5 @@
+import math
+import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,8 +14,13 @@ def browser():
     browser.quit()
 
 
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+@pytest.mark.parametrize('lesson', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
+def test_guest_should_see_login_link(browser, lesson):
+    link = f"https://stepik.org/lesson/{lesson}/step/1"
+    browser.implicitly_wait(5)
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR,"#login_link")
+    answer = math.log(int(time.time()))
+    browser.find_element(By.CSS_SELECTOR, "textarea[spellcheck]").send_keys(answer)
+    browser.find_element(By.CSS_SELECTOR, "button[class='submit-submission']").click()
+    actual_text = browser.find_element(By.CSS_SELECTOR, "pre[class]")
+    assert actual_text.text == "Correct!", "Should be equal"
