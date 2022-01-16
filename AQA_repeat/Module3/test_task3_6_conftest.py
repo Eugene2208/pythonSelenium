06ac9460ccd0f6208292979322c01fc9973cd150
“Eugene <zhenyamoroz2208@gmail.com>
2022-01-16 15:58:10 +0200

    modify conftest.py

--- AQA_repeat/Module3/test_task3_6_conftest.py
+++ AQA_repeat/Module3/test_task3_6_conftest.py
@@ -4,3 +4,5 @@ link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element_by_css_selector("#login_link")
+
+# pytest -s -v --browser_name=firefox test_task3_6_conftest.py
