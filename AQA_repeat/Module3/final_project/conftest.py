3b41950817604acb6bb10405987bdf0a8c2a0d78
“Eugene <zhenyamoroz2208@gmail.com>
2022-01-23 10:40:21 +0200

    add final project

--- AQA_repeat/Module3/final_project/conftest.py
+++ AQA_repeat/Module3/final_project/conftest.py
@@ -1,17 +1,35 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import pytest
 
 
 def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
-                     help="Choose the language for next test session.")
+                     help="Choose language for browser please")
 
-@pytest.fixture
+
+@pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intel.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
     yield browser
+    print("\nquit browser..")
     browser.quit()
