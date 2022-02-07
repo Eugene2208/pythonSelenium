eda236d24f79130031d54b8cdee36e6e01a4c1bf
zheny <zhenyamoroz2208@gmail.com>
2022-02-07 23:56:09 +0200

    something modified

--- AQA_repeat/Module4/tests/test_product_page.py
+++ AQA_repeat/Module4/tests/test_product_page.py
@@ -40,10 +40,11 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = env.get_page_link()
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
     pass
