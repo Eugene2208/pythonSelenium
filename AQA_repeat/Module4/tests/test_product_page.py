52b90d02f227b94ef51edfe5c0b5bb8d42202378
zheny <zhenyamoroz2208@gmail.com>
2022-02-07 23:43:07 +0200

    something modified

--- AQA_repeat/Module4/tests/test_product_page.py
+++ AQA_repeat/Module4/tests/test_product_page.py
@@ -3,10 +3,10 @@ import pytest
 from AQA_repeat.Module4.Environment import Environment as env
 from AQA_repeat.Module4.pages.product_page import ProductPage
 
+
 @pytest.mark.skip
 @pytest.mark.parametrize('link', [f"{env.get_page_links()}/?promo=offer{no}" for no in range(10)])
 def test_guest_can_add_product_to_basket(browser, link):
-    # link = env.get_page_links()
     prod_page = ProductPage(browser, link)
     prod_page.open()
     prod_page.add_to_basket()
@@ -14,6 +14,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     prod_page.should_be_add_to_basket()
 
 
+@pytest.mark.skip
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = env.get_page_links()
     prod_page = ProductPage(browser, link)
@@ -21,12 +22,15 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     prod_page.add_to_basket()
     prod_page.should_not_be_success_message()
 
+
 def test_guest_cant_see_success_message(browser):
     link = env.get_page_links()
     prod_page = ProductPage(browser, link)
     prod_page.open()
     prod_page.should_not_be_success_message()
 
+
+@pytest.mark.skip
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = env.get_page_links()
     prod_page = ProductPage(browser, link)
