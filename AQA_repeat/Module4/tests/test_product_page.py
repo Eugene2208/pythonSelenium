bc7cbe57b17d40d12c2bff1fbc21df378c92b072
zheny <zhenyamoroz2208@gmail.com>
2022-02-10 22:53:35 +0200

    something modified

--- AQA_repeat/Module4/tests/test_product_page.py
+++ AQA_repeat/Module4/tests/test_product_page.py
@@ -31,6 +31,7 @@ class TestUserAddToBasketFromProductPage:
         page = ProductPage(browser, link)
         page.open()
         login_page = LoginPage(browser, browser.current_url)
+        login_page.go_to_login_page()
         login_page.register_new_user(Credential.log1.value, Credential.pass1.value)
         login_page.should_be_authorized_user()
 
